f89de2091d2a937286be6a4ac2464c49b57c9fa2
ikerlandajuela <i.landajuela@koopera.local>
2017-03-05 11:20:58 +0100

    C# ThreadingBasics: Operaciones básicas con Threads
    Varios proyectos dentro de la misma solución en VS2012 para operar con
    Threads a nivel muy básico

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace ThreadingBasics
+{
+    class Program
+    {
+        static void PrintNumbers()
+        {            
+            Console.WriteLine("Starting...");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(PrintNumbers);
+            t.Start();
+            PrintNumbers();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
